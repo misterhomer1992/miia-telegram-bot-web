283b933c25d028f60f72efad7e7732a1b138ffb5
marius chik <misterhomer1992@gmail.com>
2026-05-22 16:30:54 +0300

    feat: centralize telegram url and site url constants

---
+++ src/hooks/useTelegramUrl.ts
@@ -0,0 +1,5 @@
+import { TELEGRAM_BOT_URL } from "../lib/constants";
+
+export function useTelegramUrl(): string {
+  return TELEGRAM_BOT_URL;
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const TELEGRAM_BOT_URL = "https://t.me/AIEverydayHelper_bot";
+export const SITE_URL = import.meta.env.VITE_SITE_URL ?? "https://miia.example.com";

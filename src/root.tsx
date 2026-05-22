09fc36e322789f80e367fae5f743ed9130aacf6d
marius chik <misterhomer1992@gmail.com>
2026-05-22 19:16:12 +0300

    fix(root): suppress expected html hydration warning

--- src/root.tsx
+++ src/root.tsx
@@ -25,7 +25,7 @@ export default function Root() {
   const lang = id === "landing-uk" ? "uk" : id === "landing-pl" ? "pl" : "en";
 
   return (
-    <html lang={lang} data-theme="dark">
+    <html lang={lang} data-theme="dark" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />

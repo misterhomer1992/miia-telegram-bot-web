d99731224438eea7cb805f463d7d9f96a58160cb
marius chik <misterhomer1992@gmail.com>
2026-05-22 16:13:04 +0300

    feat(root): wire global styles and font preconnect

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,21 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import type { LinksFunction } from "react-router";
+
+import resetHref from "./styles/reset.css?url";
+import tokensHref from "./styles/tokens.css?url";
+import fontsHref from "./styles/fonts.css?url";
+import animationsHref from "./styles/animations.css?url";
+import globalHref from "./styles/global.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+  { rel: "stylesheet", href: resetHref },
+  { rel: "stylesheet", href: tokensHref },
+  { rel: "stylesheet", href: fontsHref },
+  { rel: "stylesheet", href: animationsHref },
+  { rel: "stylesheet", href: globalHref },
+];
 
 export default function Root() {
   return (
